ff213e31498af0a4cd7eabae7ef62ce8e75d661b
Bolvvv <605725874@qq.com>
2018-10-18 17:31:25 +0800

    update some entity

--- back/src/main/java/org/jishionlinemall/mall/Entity/GoodsAndCart.java
+++ back/src/main/java/org/jishionlinemall/mall/Entity/GoodsAndCart.java
@@ -1,18 +1,28 @@
 package org.jishionlinemall.mall.Entity;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 //此表用于关联good和cart(多对多)
 
 @Entity
 @Table(name = "t_goods_cart")
 public class GoodsAndCart {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id",nullable = false)
+    private Long id;
     @Column(name = "gid",nullable = false)
     private Long gid;//商品id号
     @Column(name = "cid",nullable = false)
     private Long cid;//购物车id号
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     public Long getGid() {
         return gid;
--- back/src/main/java/org/jishionlinemall/mall/Entity/GoodsAndOrder.java
+++ back/src/main/java/org/jishionlinemall/mall/Entity/GoodsAndOrder.java
@@ -1,18 +1,28 @@
 package org.jishionlinemall.mall.Entity;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 //此表用于关联good和cart(多对多)
 
 @Entity
 @Table(name = "t_goods_order")
 public class GoodsAndOrder {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id",nullable = false)
+    private Long id;
     @Column(name = "gid", nullable = false)
     private Long gid;//商品id
     @Column(name = "oid", nullable = false)
     private Long oid;//订单id
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     public Long getGid() {
         return gid;
